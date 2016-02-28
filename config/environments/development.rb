255810ef3a74f599e73df287654f5764b6250fb6
Marlon Reghert <mras@cin.ufpe.br>
2016-02-28 15:06:23 -0300

    Adding email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,24 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+config.action_mailer.perform_deliveries = true # 
+
+config.action_mailer.default_url_options = { :host => 'localhost', :port => 3001 }
+config.action_mailer.delivery_method = :smtp
+#config.action_mailer.smtp_settings = {:address => 'localhost', :port => 1025} 
+config.action_mailer.smtp_settings = {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => 587,
+      # :domain               => 'gmail.com',
+      :user_name            => 'mras',
+      :password             => 'marlon123',
+
+      :authentication       => :login,
+      :enable_starttls_auto => true
+}
+
+config.action_mailer.raise_delivery_errors = true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :only => [:sessions, :registrations] #, :controllers => { :sessions => 'users/sessions' }
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :only => [:sessions, :registrations, :confirmations] #, :controllers => { :sessions => 'users/sessions' }
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
