42883ad523204bda28a12d708e1a26edd2522782
Marlon Reghert <mras@cin.ufpe.br>
2016-03-01 15:18:56 -0300

    Adding tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+ include Devise::TestHelpers
+
+  test "should redirect unlogged users to login page when try to acess home page" do
+   @request.env["devise.mapping"] = Devise.mappings[:user]
+   post :index, user: :carlos
+   assert_response :found
+   assert_redirected_to new_user_session_path
   end
+
+  test "should provide acess to home page for logged user" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    assert users(:confirmed_user).confirmed?
+    sign_in users(:confirmed_user)
+    post :index, user: users(:confirmed_user)
+    assert_response :ok
+   end 
+
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save user without email" do
+  	user = User.new 
+  	assert_not user.save
+  end
+
+  test "should not save user with email without at symbol" do
+  	user = User.new
+  	user.email = 'teste_cin.ufpe.br'
+  	assert_not user.save
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = users(:carlos).dup
+    assert_not duplicate_user.valid?
+  end
+
+  test "should not save users with duplicated email" do
+    duplicate_user = users(:carlos).dup
+    assert_not duplicate_user.save
+  end
+
+
 end
