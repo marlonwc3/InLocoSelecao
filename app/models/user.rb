99dc3ef68b29eca4720999e441008a122fef01e2
Marlon Reghert <mras@cin.ufpe.br>
2016-03-01 19:02:25 -0300

    Adding password alphanum validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+
   devise :database_authenticatable, :registerable,
          :trackable, :validatable, :confirmable
+
+  validates_format_of  :password, with: /\A([a-zA-Z]|\d)*\z/
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -147,7 +147,9 @@ Devise.setup do |config|
   # ==> Configuration for :validatable
   # Range for password length.
   config.password_length = 8..72
+  # config.password_regexp = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/
 
+  
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
